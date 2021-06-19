a6b1ac95c3eecf30f52ce638d32982c6cd5d2b91
vankayalapativimala <84719756+vankayalapativimala@users.noreply.github.com>
2021-06-19 19:17:52 +0530

    test_average.c

--- test_average.c
+++ test_average.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+#include <assert.h>
 
+float average(int [], int );
+int main(void){
+int x[]= {9,5,6,10,2,-3,4};
+assert ((average(x , 7) - 4.7142) < 0.001);
 
-int main(){
-float average(int a[],int n){
-  float average,sum=0;
-  for (int i=0;i<n;i++){
-    sum=sum+a[i];
-  }
-  average=sum/n;
-  return average;
- }
+int y[] = {5};
+assert ((average(y,1) == 5.0));
+printf("Average: passed\n");
 }
+
