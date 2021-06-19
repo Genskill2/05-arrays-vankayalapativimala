27001ecc2c84672556878c3d33ff45df2b2d9a3a
vankayalapativimala <84719756+vankayalapativimala@users.noreply.github.com>
2021-06-19 17:02:03 +0530

    solns.c

--- solns.c
+++ solns.c
@@ -1,106 +1,76 @@
 /* Enter your solutions in this file */
-#include <stdio.h>
+#include<stdio.h>
 
-float average(int a[],int n){
-  float sum=o,average;
-  for (int i=0;i<n;i++){
-    sum=sum+a[i];
-  }
-  average=sum/n;
-  return average;
-}
-
-int factors(int n, int ret[])
-{  int p=0;
-    while(n!=1)
-  {
-    for( int i=2;i<=n;i++)
-    {
-        if(isprime(i)) 
-        {
-            while (n%i==0)
-            { n=n/i;
-              ret[p]=i;
-              p++;
-            }
-        }
+int max(int arr[], int len)
+{
+      int max=arr[0];
+    for(int i=0; i<len; i++)
+    { if (arr[i]>max)
+       max=arr[i];
     }
-  }
-  return (p);
+    return max;
 }
-
-
-int isprime(int n)
-{ int i, flag = 1;
-for (i = 2; i <= n / 2; ++i) {
-    if (n % i == 0) {
-    
-      flag = 0;
-      break;
+int min(int arr[], int len)
+{
+    int min= arr[0];
+    for(int i=0; i<len; i++)
+    { if (arr[i]<min)
+       min = arr[i];
     }
-  }
- return flag;
+    return min;
 }
-
-
-
-
-
-int max(int marks[], int n){
- 
-int maximum;
-     /**n=get_int("no of integer");
-     **/
-maximum=marks[0];
-   for (int i=0;i<n;i++){
-        /* marks[i]=get_int("\n enter mark");*/
-        if(marks[i]>maximum){
-             maximum=marks[i];
-             }
-             
-             }
-         return maximum;
-      }
-
-
-
-int mode(int x[], int n)
-{   int p[20]={0};
-    int count;
-    for(int i=0;i<n;i++)
-    {   count=0;
-        for(int j=0; j<n; j++)
-        { if(x[j]==x[i])
-           {count++;}
-        }
-        p[i]=count;
+float average(int arr[], int len)
+{
+    float average, sum=0;
+    for( int i=0; i<len; i++)
+    sum+=arr[i];
+    average=sum/len;
+    return average;
+}
+int mode(int arr[], int len)
+{
+    int arrno[len];
+    int n,count=0,a;
+    for( int i=0; i<len; i++){
+    n= arr[i];
+    for(int j=0; j<len; j++)
+    {    if( arr[i]==arr[j])
+        count++;
     }
-    int index=0;
-    int mx=p[0];
-    for(int i=1;i<n;i++)
-    {
-       if(p[i]>mx)
-       {
-           mx=p[i];
-           index=i;
-       }
+    arrno[i]=count;
+    count =0;
     }
- return x[index];
+    int max=arrno[0];
+    for( int k=0;k<len;k++)
+      { if(arrno[k]>max)
+          max=arrno[k];}
+    for(a=0;a<len; a++)
+    { if(max==arrno[a])
+         break;
+     }
+     return arr[a];
 }
-
-
-int min(int a[], int n){
- int minimum;
-minimum=a[0];
-for (int i=0;i<n;i++){
-    if (a[i]<minimum){
-      minimum=a[i];
+int factors(int num, int arr[])
+{
+    int cpy=num;
+    int cnt=0;
+    int cnt1=0;
+    for (int i=2; i<num/2;i++)
+    { if(num%i==0){
+    for(int j=2; j<=i;j++)
+    {if(i%j==0)
+    cnt+=1;
     }
+    if(cnt==1){
+    while(cpy%i==0){
+    arr[cnt1]=i;
+    cpy/=i;
+    cnt1+=1;
+    }
+    cpy=num;}
+    cnt=0;
+    }
+    else
+    continue;}
+    return cnt1;
 }
-return minimum;
-}
-
-
-
-
- 
