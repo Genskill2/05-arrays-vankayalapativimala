2e39a7def29d9cda0d27f97a911033470656f650
vankayalapativimala <84719756+vankayalapativimala@users.noreply.github.com>
2021-06-19 15:13:24 +0530

    test_max.c

--- test_max.c
+++ test_max.c
@@ -2,7 +2,7 @@
 #include <assert.h>
 
 
-int max(int [], int);
+int max(int marks[], int n);
 
 int main(void) {
   int x[] = {9,5,6,10,2,-3,4};
@@ -12,3 +12,18 @@ int main(void) {
   assert (max(y, 1) == 5);
   printf("Max: passed\n");
   }
+int max(int marks[], int n){
+ 
+int maximum;
+     /**n=get_int("no of integer");
+     **/
+maximum=marks[0];
+   for (int i=0;i<n;i++){
+        /* marks[i]=get_int("\n enter mark");*/
+        if(marks[i]>maximum){
+             maximum=marks[i];
+             }
+             
+             }
+         return maximum;
+      }
